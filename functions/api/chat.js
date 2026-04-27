cab7665bc40a97d9d34bbeb575e1131d3b5c9774
awlagh <hola@awla.live>
2026-04-27 17:59:33 -0600

    Update chat.js

--- functions/api/chat.js
+++ functions/api/chat.js
@@ -1,7 +1,21 @@
-export async function onRequestPost(context) {
+export async function onRequest(context) {
+  if (context.request.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type'
+      }
+    });
+  }
+ 
+  if (context.request.method !== 'POST') {
+    return new Response('Method Not Allowed', { status: 405 });
+  }
+ 
   const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
     'Content-Type': 'application/json'
   };
  
@@ -68,16 +82,5 @@ Empieza con la primera pregunta solamente.`;
       headers: corsHeaders
     });
   }
-}
- 
-export async function onRequestOptions() {
-  return new Response(null, {
-    status: 204,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type'
-    }
-  });
 }
  
